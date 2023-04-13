0f8998761434ee5aca001bd82689673d5fac017a
xxmrcutxx@gmail.com <xxmrcutxx@gmail.com>
2023-04-14 01:12:54 +0200

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { POKEMONS } from "./mock-pokemon-list";
+import { Pokemon } from "./pokemon";
+
+const ERROR_POKEMON_NOT_FOUND = "Le Pokémon demandé n'existe pas.";
 
 @Component({
   selector: "app-root",
-  template: ` <h1>Welcome to {{ pokemons[1] }}!</h1>`,
+  templateUrl: "app.component.html",
 })
-export class AppComponent {
-  title = "application de pokémons";
-  pokemons = ["Bulbizarre", "Salamèche", "Carapuce"];
+export class AppComponent implements OnInit {
+  pokemonList = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id == +pokemonId
+    );
+
+    if (pokemon) {
+      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.error(ERROR_POKEMON_NOT_FOUND);
+      this.pokemonSelected = pokemon;
+    }
+  }
 }
