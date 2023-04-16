31db62646cfbb84e68ab93893ecf35610388307b
xxmrcutxx@gmail.com <xxmrcutxx@gmail.com>
2023-04-16 18:15:09 +0200

    4rd commit

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,16 +1,21 @@
 import { Component } from "@angular/core";
-import { POKEMONS } from "../mock-pokemon-list";
 import { Pokemon } from "../pokemon";
 import { Router } from "@angular/router";
+import { PokemonService } from "../pokemon.service";
 
 @Component({
   selector: "app-list-pokemon",
   templateUrl: "./list-pokemon.component.html",
 })
 export class ListPokemonComponent {
-  pokemonList = POKEMONS;
+  pokemonList: Pokemon[];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.pokemonList = this.pokemonList = this.pokemonService.getPokemonList();
+  }
+
   goToPokemon(pokemon: Pokemon) {
     this.router.navigate(["/pokemon", pokemon.id]);
   }
